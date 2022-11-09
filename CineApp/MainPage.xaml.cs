ab31b475ea4646b26e43e7eaedf6022fe113d32d
Maissa <Maissa@MAISSA>
2022-11-09 15:45:30 -0600

    Add project files.

---
+++ CineApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CineApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSesion_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistro_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
